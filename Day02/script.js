32d273e2494fb09357e5879d480771f0b9b43154
abhishek2002s <linkedin23356abhi@gmail.com>
2024-02-10 00:58:18 +0530

    Day02

---
+++ Day02/script.js
@@ -0,0 +1,22 @@
+//variabel practic
+let accountEmail = "abhi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+accountEmail = "soni@gmail.com"
+accountPassword = "2345"
+accountCity = "Mumbai"
+
+console.table([accountEmail,accountPassword,accountCity,accountState]);
+
+
+// Data types exmaple
+//alert we are using nodejs not browser
+let name = "abhi"
+let age = 20
+let isLoggedIn = false
+
+console.log(typeof("sabh"));
+console.log(typeof(undefined));
+console.log(typeof(null));
